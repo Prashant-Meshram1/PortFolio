b628e8d270cbd2f98c1011db0d4a6f37f7217563
prashant meshram <prashantmeshram1437@gmail.com>
2024-08-08 22:34:40 +0530

    Final Changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,9 +7,14 @@ import { RiContactsBook3Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { CiDark } from "react-icons/ci";
 import { MdDarkMode } from "react-icons/md";
+import { useState } from "react";
 const Navbar = ({ theme, setTheme }) => {
+    const [itemSelect, setItemsSelect] = useState("home")
     const toggle_mode = () => {
         theme == true ? setTheme(false) : setTheme(true)
+    }
+    const clickHandle = (items) => {
+        setItemsSelect(items)
     }
     return (
 
@@ -20,32 +25,34 @@ const Navbar = ({ theme, setTheme }) => {
             </div >
             <ul>
                 <div className="flex  justify-end relative top-[-47px] ">
-                    <Link to=".">
-                        <div className="bg-cyan-700  m-4 h-16 w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer">
+
+
+
+                    <Link to="." onClick={() => clickHandle("home")}>
+                        <div className={`${itemSelect === "home" ? ' h-16  m-4  w-16 bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer' : ' bg-cyan-700 h-16  m-4  w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer'}`}>
                             <IoIosHome className="h-6 w-6 text-white" />
-                            <p className="text-white font-round  text-[13px]" >  Home </p>
+                            <p className="text-white font-round  text-[13px]  ">Home </p>
                         </div>
                     </Link>
-                    <Link to="resume">
-                        <div className=" bg-cyan-700  m-4  h-16 w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer">
+                    <Link to="resume" onClick={() => clickHandle("resume")}>
+                        <div className={`${itemSelect === "resume" ? ' h-16  m-4  w-16 bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer' : ' bg-cyan-700 h-16  m-4  w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer'}`}>
                             <MdLibraryBooks className="h-6 w-6 text-white" />
-                            <p className="text-white font-round text-[13px]  ">Resume</p>
+                            <p className="text-white font-round  text-[13px]  ">Resume </p>
                         </div>
                     </Link>
-
-
-                    <Link to="projects">
-                        <div className=" bg-cyan-700 h-16  m-4  w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer">
+                    <Link to="projects" onClick={() => clickHandle("projects")}>
+                        <div className={`${itemSelect === "projects" ? ' h-16  m-4  w-16 bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer' : ' bg-cyan-700 h-16  m-4  w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer'}`}>
                             <GoProjectSymlink className="h-6 w-6 text-white" />
                             <p className="text-white font-round  text-[13px]  ">Projects </p>
                         </div>
                     </Link>
-                    <Link to="contacts">
-                        <div className="h-16 w-16  m-4 bg-cyan-700 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer ">
+                    <Link to="contacts" onClick={() => clickHandle("contacts")}>
+                        <div className={`${itemSelect === "contacts" ? ' h-16  m-4  w-16 bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer' : ' bg-cyan-700 h-16  m-4  w-16 hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476] rounded-xl flex flex-col justify-center items-center cursor-pointer'}`}>
                             <RiContactsBook3Line className="h-6 w-6 text-white" />
-                            <p className="text-white font-round text-[13px] ">Contacts</p>
+                            <p className="text-white font-round  text-[13px]  ">Projects </p>
                         </div>
                     </Link>
+
                     <div className="relative  top-5 right-1 ">
                         <button onClick={toggle_mode} className={` bg-cyan-700 rounded-full hover:bg-gradient-to-r from-[#FA5252] to-[#DD2476]  mr-10 mt-1 h-12 w-12 ${theme ? true : false}`}>
                             {theme ? <CiDark className="h-8 w-8 ml-[10px] text-white " /> : <MdDarkMode className="h-8 w-8 ml-[10px] " />}
@@ -57,3 +64,5 @@ const Navbar = ({ theme, setTheme }) => {
     )
 }
 export default Navbar;
+
+
